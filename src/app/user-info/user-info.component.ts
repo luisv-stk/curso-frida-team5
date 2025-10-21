e0bffc854acb2c785a1630c212206644184d2ed2
pablo.jimenezc@softtek.com <pablo.jimenezc@softtek.com>
2025-10-21 10:38:55 +0200

    Feat: Prueba permisos subida. Componente user-info

---
+++ src/app/user-info/user-info.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-user-info',
+  imports: [MatIconModule],
+  standalone:true,
+  templateUrl: './user-info.component.html',
+  styleUrl: './user-info.component.css'
+})
+export class UserInfoComponent {
+
+}
